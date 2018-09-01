93cbafad093b120cf3e0687a6213fb2704d0ca90
cipepser <respepic@gmail.com>
2018-09-02 00:58:13 +0900

    implement server push

--- server.go
+++ server.go
@@ -5,9 +5,9 @@ import (
 	"net/http"
 )
 
-func main()  {
+func main() {
 	srv := &http.Server{
-		Addr: ":8080",
+		Addr:    ":8080",
 		Handler: http.HandlerFunc(handle),
 	}
 
@@ -17,5 +17,23 @@ func main()  {
 
 func handle(w http.ResponseWriter, r *http.Request) {
 	log.Printf("Got connection: %s", r.Proto)
+
+	if r.URL.Path == "/2nd" {
+		log.Println("Handling 2nd")
+		w.Write([]byte("Hello Again!"))
+		return
+	}
+
+	log.Println("Handling 1st")
+	pusher, ok := w.(http.Pusher)
+	if !ok {
+		log.Println("Can't push to client")
+	} else {
+		err := pusher.Push("/2nd", nil)
+		if err != nil {
+			log.Printf("Failed push: %v", err)
+		}
+	}
+
 	w.Write([]byte("Hello"))
 }
